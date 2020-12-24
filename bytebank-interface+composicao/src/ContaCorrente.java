9c277c8e35ec910398b6bd331a348496bfc8dc15
Carlos-kaspa <carloslimatwg@gmail.com>
2020-12-24 19:56:58 -0300

    uso de herança e classes abstratas no bytebank

--- bytebank-interface+composicao/src/ContaCorrente.java
+++ bytebank-interface+composicao/src/ContaCorrente.java
@@ -4,4 +4,15 @@ public class ContaCorrente extends Conta {
 	public ContaCorrente(String titular) {
 		super(titular);
 	}
+	
+	@Override
+	public void saca(double valor) {
+		double taxa = 0.90;
+		double valorFinal = valor + taxa;
+		this.saca(valorFinal);
+	}
+	
+
+	
 }
+
